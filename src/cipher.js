36b7ea156af0a7966d4bb799a4915cd2fd62d8aa
Mariia <maria.bookvoed@gmail.com>
2019-04-02 11:50:30 -0300

    poner cifrado dentro de cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,40 @@
+let cifrado;
+let decifrado;
+let charCode;
+let charCodeNew;
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+    encode: (offset, string) => {
+        for (let i = 0; i < string.length; i++) {
+            //obtenemos nuer de cahrcode por cada caracter en texto ingresado
+            charCode = string.charCodeAt(i);
+            //en caso si numero de charcode esta entre 65 hasta 90
+            if (charCode >= 65 && charCode <= 90) {
+                //vamos a usar formula que va a codificar(osea devolver nuevo number)
+                charCodeNew = (charCode - 65 + offset) % 26 + 65;
+                //y despues obtenemos el significado de nuevo number
+                charCodeNew = String.fromCharCode(charCodeNew);
+                // aqui estamos agregando cada nuevo caracter cifrado
+                cifrado = cifrado + charCodeNew;
+            } else {
+                //en caso si number de cararcter no esta dentro 65 -90 no lo vamos a cifrar(por mientras)
+                charCode = String.fromCharCode(charCode);
+                cifrado = cifrado + charCode;
+            }
+        }
+        return (cifrado);
+    },
+    decode: (offset, string) => {
+        for (let i = 0; i < string.length; i++) {
+            charCode = string.charCodeAt(i);
+            if (charCode >= 65 && charCode <= 90) {
+                charCodeNew = (charCode + 65 - offset) % 26 + 65;
+                charCodeNew = String.fromCharCode(charCodeNew);
+                decifrado = decifrado + charCodeNew;
+            } else {
+                charCode = String.fromCharCode(charCode);
+                decifrado = decifrado + charCode;
+            }
+        }
+        return (decifrado);
+    }
 };
